ffc62cd400a8ff769289127a0e0744dbc09fe92a
Lucas <74873871+lucascampardo@users.noreply.github.com>
2023-05-27 17:49:33 -0300

    Seguindo com os estudos em Java

---
+++ EstruturaSequencial/EntradaDeDados.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EntradaDeDados {
+
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); // Fazer um sc.close() quando não for mais usar o Scanner
+ 
+        String x;
+        int y;
+        double z;
+
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+
+        System.out.println("Dados digitados:\n" + x);
+        System.out.println(y);
+        System.out.println(z);
+
+        sc.close();
+
+
+    }
+}
